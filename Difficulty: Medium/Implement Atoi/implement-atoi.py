77fb64a61703869712ab657290fbd9c3c1740f08
Venkat <venky1908112@gmail.com>
2026-04-16 09:59:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Implement Atoi/implement-atoi.py
+++ Difficulty: Medium/Implement Atoi/implement-atoi.py
@@ -1,22 +1,20 @@
 class Solution:
     def myAtoi(self, s):
-        # code here
         s = ''.join(s.split())
         n = len(s)
+        sign = 1
         start_idx = 0
-        s_pos = 1
         if s[0] == '-':
-            s_pos = -1
+            sign = -1
             start_idx = 1
         if s[0] == '+':
             start_idx = 1
-        start = start_idx
-        while start_idx < n and s[start_idx].isdigit():
-            start_idx += 1
-        num = int(s[start : start_idx]) * s_pos
+        end_idx = start_idx
+        while end_idx < n and s[end_idx].isdigit():
+            end_idx += 1
+        num = int(s[start_idx : end_idx]) * sign
         if num > 2147483647:
             return 2147483647
         if num < -2147483648:
             return -2147483648
         return num
-            
